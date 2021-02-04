f46582285ee89243c132bea67bf1be37b40a65e3
AleksandrV <kolomel@yandex.ru>
2021-02-04 14:28:24 +0300

    add User component, upd App, add getUsers function

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,30 @@
-
+import React from 'react';
+import usersArr from './users';
+import User from './User';
 
 function App() {
+
+  const [users, setUsers] = React.useState([]);
+
+  function getUsers() {
+    const arr = usersArr.slice();
+    setUsers(arr);
+    // api.getInitialCards()
+    //   .then((res) => { const arr = res.slice(); setCards(arr) })
+    //   .catch((err) => { console.log(`Ошибка: ${err}`); })
+  }
+
+  React.useEffect(() => {
+    getUsers()
+  }, []);
+  
   return (
-    <div className="App">
+    <div className="root">
       <header className="App-header">
-       <div>Users</div>
       </header>
+      {users.map((user, i) => {
+          return <User key={user.id} user={user}/>
+      })}
     </div>
   );
 }
---
+++ src/components/User.js
@@ -0,0 +1,47 @@
+import React from 'react';
+//import { CurrentUserContext } from '../contexts/CurrentUserContext';
+
+const User = React.memo((props) => {
+
+    const user = props.user;
+
+  return (
+    <>
+      <div className="list-item">
+        <div className="">Имя: {user.name}</div>
+        <div className="">Ник: {user.username}</div>
+        <div className="">E-mail: {user.email}</div>
+        <div className="">Адрес: {user.address.street}, {user.address.suite}, {user.address.city}, {user.address.zipcode}</div>
+        <div className="">Номер телефона: {user.phone}</div>
+        <div className="">Веб-сайт: {user.website}</div>
+        <div className="">Компания: {user.company.name}, {user.company.catchPhrase}, {user.company.bs}</div>
+      </div>
+    </>
+  )
+})
+
+export default User; 
+
+// {
+//     "id": 1,
+//     "name": "Leanne Graham",
+//     "username": "Bret",
+//     "email": "Sincere@april.biz",
+//     "address": {
+//       "street": "Kulas Light",
+//       "suite": "Apt. 556",
+//       "city": "Gwenborough",
+//       "zipcode": "92998-3874",
+//       "geo": {
+//         "lat": "-37.3159",
+//         "lng": "81.1496"
+//       }
+//     },
+//     "phone": "1-770-736-8031 x56442",
+//     "website": "hildegard.org",
+//     "company": {
+//       "name": "Romaguera-Crona",
+//       "catchPhrase": "Multi-layered client-server neural-net",
+//       "bs": "harness real-time e-markets"
+//     }
+//   },
