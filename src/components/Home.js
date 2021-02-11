be56a972067a8ce7922580b2720834022da90ebb
AleksandrV <kolomel@yandex.ru>
2021-02-11 15:05:39 +0300

    fix path in Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,9 +5,9 @@ function Home() {
   return (
     <div className="home-container">
       <h1 className="home-container__title">Приожение для просмотра постов, льбомов и информации о пользователях</h1>
-      <Link className="button home-container__button" to="/users">Пользователи</Link>
-      <Link className="button home-container__button" to="/posts">Посты</Link>
-      <Link className="button home-container__button" to="/albums">Альбомы</Link>
+      <Link className="button home-container__button" to="/SPA-users-albums-and-posts/users">Пользователи</Link>
+      <Link className="button home-container__button" to="/SPA-users-albums-and-posts/posts">Посты</Link>
+      <Link className="button home-container__button" to="/SPA-users-albums-and-posts/albums">Альбомы</Link>
     </div>
   );
 }
