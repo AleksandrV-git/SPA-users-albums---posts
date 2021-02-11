a78226945b32546043b0e6e2deec01afa631d57c
AleksandrV <kolomel@yandex.ru>
2021-02-11 16:11:06 +0300

    fix Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 function Home() {
   return (
     <div className="home-container">
-      <h1 className="home-container__title">Приожение для просмотра постов, льбомов и информации о пользователях</h1>
+      <h1 className="home-container__title">Приложение для просмотра постов, альбомов и информации о пользователях</h1>
       <Link className="button home-container__button" to="/users">Пользователи</Link>
       <Link className="button home-container__button" to="/posts">Посты</Link>
       <Link className="button home-container__button" to="/albums">Альбомы</Link>
